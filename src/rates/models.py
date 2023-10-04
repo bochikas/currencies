3b0e1b0bd8107dac7aad46a55ec8a5c55d91da44
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-10-04 17:40:15 +0500

    Rate model created

---
+++ src/rates/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Rate(models.Model):
+    """Курс валюты."""
+
+    char_code = models.CharField(verbose_name='код валюты', max_length=3)
+    date = models.DateField(verbose_name='дата')
+    value = models.DecimalField(verbose_name='курс', max_digits=10, decimal_places=4)
+
+    class Meta:
+        verbose_name = 'курс валют'
+        verbose_name_plural = 'курсы валют'
+        ordering = ('-date',)
