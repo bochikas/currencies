760785e33e48d344986f636cbb9e646fe99ab6f8
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-10-04 13:48:43 +0500

    user model updated

--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     """Пользователь."""
 
+    username = models.CharField(verbose_name=_('username'), max_length=150, null=True, blank=True)
+    email = models.EmailField(verbose_name=_('email address'), unique=True)
     register_date = models.DateField(verbose_name='Дата регистрации', auto_now_add=True)
     deleted = models.BooleanField(default=False)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     class Meta(AbstractUser.Meta):
-        ordering = ('username',)
+        ordering = ('email',)
