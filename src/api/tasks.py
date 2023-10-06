9c7a7e1af37d0f33bba67c716c7812e46199768c
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-10-06 17:55:38 +0500

    load_currency_rates celery task updated according to model changes

--- src/api/tasks.py
+++ src/api/tasks.py
@@ -3,8 +3,9 @@ import datetime
 import requests
 from celery import shared_task
 from django.conf import settings
+from django.db import transaction
 
-from rates.models import Rate
+from rates.models import Currency, Rate
 
 
 class CBRConnectionException(Exception):
@@ -13,25 +14,34 @@ class CBRConnectionException(Exception):
 
 @shared_task(ignore_result=True, autoretry_for=(CBRConnectionException,), retry_backoff=True)
 def load_currency_rates():
-    """Загрузка дневных котировок ЦБ РФ."""
+    """Загрузка дневных котировок ЦБ РФ за последние 30 дней."""
 
     url = settings.CBR_DAILY_RATES_URL
     today = datetime.date.today()
     to_create = list()
-    for _ in range(30):
-        if Rate.objects.filter(date=today).exists():
+    to_update = list()
+
+    with transaction.atomic():
+        for _ in range(30):
+            if Rate.objects.filter(date=today).exists():
+                today = today - datetime.timedelta(days=1)
+                continue
+
+            response = requests.get(url=url)
+            if response.status_code != 200:
+                raise CBRConnectionException(response.content.decode('utf-8'))
+
+            all_rates = response.json()
+            currency_values = all_rates.get('Valute')
+            for key, value in currency_values.items():
+                currency, _ = Currency.objects.get_or_create(char_code=key.upper())
+                to_create.append(Rate(date=today, currency=currency, value=value.get('Value')))
+                if not currency.name:
+                    currency.name = value.get('Name')
+                    to_update.append(currency)
+
             today = today - datetime.timedelta(days=1)
-            continue
-        response = requests.get(url=url)
-        if response.status_code != 200:
-            raise CBRConnectionException(response.content.decode('utf-8'))
+            url = all_rates.get('PreviousURL').strip('\\/').replace('\\/', '/').replace('www.', 'https://')
 
-        all_rates = response.json()
-        currency_values = all_rates.get('Valute')
-        for key, value in currency_values.items():
-            to_create.append(Rate(date=today, char_code=key, value=value.get('Value')))
-
-        today = today - datetime.timedelta(days=1)
-        url = all_rates.get('PreviousURL').strip('\\/').replace('\\/', '/').replace('www.', 'https://')
-
-    Rate.objects.bulk_create(to_create)
+        Rate.objects.bulk_create(to_create)
+        Currency.objects.bulk_update(to_update, fields=['name'])
