a3ec6d359f763089d1a3d7dfe19f729e847c7498
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-10-07 09:34:28 +0500

    RateFilter created

---
+++ src/api/filters.py
@@ -0,0 +1,28 @@
+from django_filters import rest_framework as filters
+
+from rates.models import Rate
+
+
+class RateFilter(filters.FilterSet):
+    """Фильтр обзоров."""
+
+    threshold = filters.NumberFilter(method='filter_threshold')
+    date_from = filters.DateFilter(method='filter_date_from')
+    date_to = filters.DateFilter(method='filter_date_to')
+
+    class Meta:
+        fields = ('threshold', 'date_from', 'date_to')
+        model = Rate
+
+    def filter_threshold(self, qs, name, value):
+        return qs
+
+    def filter_date_from(self, qs, name, value):
+        if value:
+            return qs.filter(date__gte=value)
+        return qs
+
+    def filter_date_to(self, qs, name, value):
+        if value:
+            return qs.filter(date__lte=value)
+        return qs
